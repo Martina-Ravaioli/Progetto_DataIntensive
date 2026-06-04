d3e7c21d0912384788427c1d9bbffc7cd746a7fe
Martina-Ravaioli <martina.ravaioli6@studio.unibo.it>
2026-06-04 10:43:46 +0200

    Add files via upload

---
+++ Progetto/README.txt
@@ -0,0 +1,3 @@
+Per avviare l'app lato server lanciare da dentro la directory del progetto il comando: py -m flask run 
+
+poi per accedere all'app web utilizzare l'url: http://127.0.0.1:5000/
---
+++ Progetto/predict.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+# Home page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# Predizione
+@app.route("/predict")
+def predict():
+    try:
+        # Lettura input dal form
+        inputs = [
+            float(request.args["daily_work_hours"]),
+            float(request.args["sleep_hours"]),
+            float(request.args["caffeine_intake"]),
+            float(request.args["bugs_per_day"]),
+            float(request.args["meetings_per_day"]),
+            float(request.args["exercise_hours"])
+        ]
+
+        # Caricamento modello
+        with app.open_resource("model.bin", "rb") as f:
+            model = pickle.load(f)
+
+        # Predizione
+        output = model.predict([inputs])[0]
+
+        # Mapping classi
+        mapping = {
+            0: "Low",
+            1: "Medium",
+            2: "High"
+        }
+
+        response = mapping.get(output, "Errore")
+
+        return render_template("predict.html", resp=response)
+
+    except Exception as e:
+        return f"Errore: {e}"
